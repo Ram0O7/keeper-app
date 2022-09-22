c2700db27753c5b1d2f82204a5c68c8b586f6114
Ramkrishn <ram706860@gmail.com>
2022-09-22 22:55:16 +0530

    Updates

---
+++ precache-manifest.0edc794e01fe9a07feb8a3f11b27aa45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6340136570e8bf1dd4f",
+    "url": "/keeper-app/static/css/main.0796cbbb.chunk.css"
+  },
+  {
+    "revision": "e6340136570e8bf1dd4f",
+    "url": "/keeper-app/static/js/main.e6340136.chunk.js"
+  },
+  {
+    "revision": "2c1d8063e8cf2864166f",
+    "url": "/keeper-app/static/js/1.2c1d8063.chunk.js"
+  },
+  {
+    "revision": "6a6139f4d4003c4e2e4e",
+    "url": "/keeper-app/static/js/runtime~main.6a6139f4.js"
+  },
+  {
+    "revision": "3a28d2b41af2e75abe0adb4476e3dbe2",
+    "url": "/keeper-app/index.html"
+  }
+];
